4981fe33c284791714e2000e5dbacabc261ead47
NurayatTogysbay <n_togysbay@kbtu.kz>
2025-04-05 07:17:33 +0500

    Delete Classes directory

--- Classes/task1.py
+++
@@ -1,9 +0,0 @@
-class Myclass:
-    def getString():
-        a = input("Enter a string: ")
-        return a
-        #self.st = a
-    def printString(a):
-        print(a.upper())
-var = Myclass.getString()
-Myclass.printString(var)
